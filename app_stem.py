49b6beb344369f8321ca82470b5ead4bac539b6f
IgranAli <igranabdi18@gmail.com>
2025-02-07 13:22:50 +0200

    Update app_stem.py

--- app_stem.py
+++ app_stem.py
@@ -1,6 +1,6 @@
 import streamlit as st
- import pandas as pd
- import numpy as np
+import pandas as pd
+import numpy as np
  
  # Set page title
  st.set_page_config(page_title="Researcher Profile and STEM Data Explorer", layout="wide")
